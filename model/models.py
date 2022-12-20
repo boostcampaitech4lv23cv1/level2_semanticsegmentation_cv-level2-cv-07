3bbc071ea6ce79de67ed7c1cf5b98ba9c728f9e9
juye-ops <kjye.ops@gmail.com>
2022-12-20 19:14:30 +0000

    FIX: forward 함수 수정

--- model/models.py
+++ model/models.py
@@ -6,10 +6,11 @@ import torchvision.models as models
 class CustomModel(nn.Module):
     def __init__(self):
         super().__init__()
-        model = models.segmentation.fcn_resnet50(pretrained=True)
-        model.classifier[4] = nn.Conv2d(512, 11, kernel_size=1)
+        self.model = models.segmentation.fcn_resnet50(pretrained=False)
+        self.model.classifier[4] = nn.Conv2d(512, 11, kernel_size=1)
 
     def forward(self, x):
+        x = self.model(x)
         return x
     
 if __name__ == "__main__":
