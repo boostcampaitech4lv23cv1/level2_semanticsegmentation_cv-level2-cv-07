a527be02a9b9880b73c2a0a0d2a55ba351150095
SeongSuKim95 <pand_a@naver.com>
2022-12-21 18:10:38 +0000

    ADD: MMsegmentation

---
+++ mmsegmentation/configs/_seongsu_/deeplabv3_plus.py
@@ -0,0 +1,6 @@
+_base_ = [
+    './_base_/models/deeplabv3plus_r50-d8.py',
+    './_base_/datasets/coco_segmentation.py',
+    './_base_/schedules/schedule_20k.py',
+    './_base_/default_runtime.py'
+]
