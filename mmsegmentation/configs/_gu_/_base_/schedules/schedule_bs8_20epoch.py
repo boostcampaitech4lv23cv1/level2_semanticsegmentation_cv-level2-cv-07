682b39036b17e77ff207b422aad0329f9cf512a6
Gu Lee <99sphere@gmail.com>
2022-12-22 07:56:13 +0000

    FIX: standard scheduler

--- mmsegmentation/configs/_gu_/_base_/schedules/schedule_bs8_20epoch.py
+++ mmsegmentation/configs/_gu_/_base_/schedules/schedule_bs8_20epoch.py
@@ -4,6 +4,6 @@ optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
-runner = dict(type='IterBasedRunner', max_iters=160000)
-checkpoint_config = dict(by_epoch=False, interval=16000)
+runner = dict(type='IterBasedRunner', max_iters=6500)
+checkpoint_config = dict(by_epoch=False, interval=2000)
 evaluation = dict(interval=200, metric='mIoU', save_best="mIoU", pre_eval=True)
