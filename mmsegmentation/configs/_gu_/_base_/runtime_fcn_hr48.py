e857b911fc1025ef7cbdf7ffece30a030b65d0c4
Gu Lee <99sphere@gmail.com>
2022-12-26 02:16:29 +0000

    add: runtime for fcn HRNet 48

---
+++ mmsegmentation/configs/_gu_/_base_/runtime_fcn_hr48.py
@@ -0,0 +1,21 @@
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook', by_epoch=False),
+        dict(type='TensorboardLoggerHook'),
+        # dict(type='WandbLoggerHook',
+        #      init_kwargs=dict(
+        #          project='MMSeg',
+        #          entity='CV07',
+        #          reinit=True,
+        #      ))
+        # dict(type='PaviLoggerHook') # for internal services
+    ])
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = '/opt/ml/input/code/mmsegmentation/pretrained/fcn_hr48_512x512_40k_voc12aug_20200613_222111-1b0f18bc.pth'
+resume_from = None
+workflow = [('train', 1), ('val', 1)]
+cudnn_benchmark = True
