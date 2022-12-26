e9f8171ab8a79e302d08d61a49100e63e08a725e
Gu Lee <99sphere@gmail.com>
2022-12-26 02:23:10 +0000

    REFACTOR: refactor config/_gu_/schedules dir structure. Separate Epoch based runner, Iter based runner dir

---
+++ mmsegmentation/configs/_gu_/_base_/schedules/epoch_based_runner/schedule_20epoch.py
@@ -0,0 +1,10 @@
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
+# runtime settings
+# Epoch based learner
+runner = dict(type="EpochBasedRunner", max_epochs=20)
+checkpoint_config = dict(interval=101)
+evaluation = dict(interval=1, metric="mIoU", save_best='mIoU',pre_eval=True)
