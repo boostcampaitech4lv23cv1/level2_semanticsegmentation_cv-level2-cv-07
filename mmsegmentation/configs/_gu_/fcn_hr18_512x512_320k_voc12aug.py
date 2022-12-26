8a1c32a4a8293220f1013f2237678ce1a1703bc8
Gu Lee <99sphere@gmail.com>
2022-12-26 02:18:09 +0000

    REFACTOR: refactor config/_gu_ dir structure

--- mmsegmentation/configs/_gu_/fcn_hr18_512x512_320k_voc12aug.py
+++
@@ -1,5 +0,0 @@
-_base_ = [
-    '/opt/ml/input/code/mmsegmentation/configs/_gu_/_base_/models/fcn_hr18.py', '/opt/ml/input/code/mmsegmentation/configs/_gu_/_base_/datasets/coco_segmentation.py',
-    '/opt/ml/input/code/mmsegmentation/configs/_gu_/_base_/fcn_hr48_runtime.py', '/opt/ml/input/code/mmsegmentation/configs/_gu_/_base_/schedules/schedule_320k.py'
-]
-model = dict(decode_head=dict(num_classes=11))
