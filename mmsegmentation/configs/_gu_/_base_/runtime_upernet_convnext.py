d69ea75c7055251f5e15fee29408c01133bb8d9c
Gu Lee <99sphere@gmail.com>
2022-12-27 09:00:26 +0000

    FEAT: exp setting for upernet convnext xlarge

---
+++ mmsegmentation/configs/_gu_/_base_/runtime_upernet_convnext.py
@@ -0,0 +1,22 @@
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook', by_epoch=False),
+        dict(type='TensorboardLoggerHook'),
+        # dict(type='WandbLoggerHook',
+        #      init_kwargs=dict(
+        #          project='MMSeg',
+        #          entity='CV07',
+        #          reinit=True,
+        #      ))
+        # dict(type='PaviLoggerHook') # for internal services
+    ])
+
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = "/opt/ml/input/code/mmsegmentation/pretrained/upernet_convnext_xlarge_fp16_640x640_160k_ade20k_20220226_080344-95fc38c2.pth"
+resume_from = None
+workflow = [('train', 1), ('val', 1)]
+cudnn_benchmark = True
