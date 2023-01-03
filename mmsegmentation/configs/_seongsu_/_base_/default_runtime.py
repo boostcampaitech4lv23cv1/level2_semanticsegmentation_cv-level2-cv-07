5af2e506085bc0b8df6a65209fcc29259ca28d08
SeongSuKim95 <pand_a@naver.com>
2023-01-03 06:34:07 +0000

    ADD :  beit config fixed

--- mmsegmentation/configs/_seongsu_/_base_/default_runtime.py
+++ mmsegmentation/configs/_seongsu_/_base_/default_runtime.py
@@ -15,7 +15,10 @@ log_config = dict(
 # yapf:enable
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
+<<<<<<< Updated upstream
 load_from = '/opt/ml/input/level2_semanticsegmentation_cv-level2-cv-07/mmsegmentation/pretrained/beit_large_patch16_224_pt22k_ft22k_convert.pth'
+=======
+>>>>>>> Stashed changes
 resume_from = None
 workflow = [('train', 1)]
 cudnn_benchmark = True
