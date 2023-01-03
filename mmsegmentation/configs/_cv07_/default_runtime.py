5eff8823e0784b34b2a93ba13ff37815b80aa3e7
SeongSuKim95 <pand_a@naver.com>
2023-01-03 11:05:49 +0000

    "ADD : Beit final experiments config"

--- mmsegmentation/configs/_cv07_/default_runtime.py
+++ mmsegmentation/configs/_cv07_/default_runtime.py
@@ -15,7 +15,6 @@ log_config = dict(
 # yapf:enable
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = '/opt/ml/input/level2_semanticsegmentation_cv-level2-cv-07/mmsegmentation/pretrained/pspnet_r50-d8_512x512_80k_ade20k_20200615_014128-15a8b914.pth'
 resume_from = None
 workflow = [('train', 1), ('val', 1)]
 cudnn_benchmark = True
